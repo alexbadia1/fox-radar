adbe935d8d78c761a91f0cc02e894d0d9b98bdf8
alexbadia1 <badia.alex.h@gmail.com>
2021-08-05 16:42:55 -0400

    Implemented boiler plate code to give a comfty message to the user about when an event started and ended.

--- lib/logic/cubits/fetch_event_state.dart
+++ lib/logic/cubits/fetch_event_state.dart
@@ -16,13 +16,12 @@ class FetchFullEventSuccess extends FetchFullEventState {
   DateTime start;
   String endSubtitle;
   String startSubtitle;
-  String formattedEndDate;
-  String formattedStartDate;
 
-  FetchFullEventSuccess({@required this.eventModel}) : assert(eventModel != null) {
+  FetchFullEventSuccess({@required this.eventModel})
+      : assert(eventModel != null) {
     end = this.eventModel.rawEndDateAndTime;
     start = this.eventModel.rawStartDateAndTime;
-  }// FetchFullEventSuccess
+  } // FetchFullEventSuccess
 
   void formatEventDatesAndTimes() {
     final event = this.eventModel;
@@ -37,52 +36,135 @@ class FetchFullEventSuccess extends FetchFullEventState {
         0,
         0);
 
-    if (myCurrent.difference(this.start).inDays == 0) {
-      this.formattedStartDate = 'today';
-    } else if (myCurrent.difference(this.start).inDays == 1) {
-      this.formattedStartDate = 'yesterday';
-    } else if (myCurrent.difference(this.start).inDays == -1) {
-      this.formattedStartDate = 'tomorrow';
-    } else {
-      this.formattedStartDate = 'on ${event.startDate}';
-    }
+    // Format start times
+
+    // Compare start time to now
+    final int startDiffInDays = myCurrent.difference(this.start).inDays;
+    final int startDiffInHours = myCurrent.difference(this.start).inHours;
+    final int startDiffInMinutes = myCurrent.difference(this.start).inMinutes;
+
+    // Event is today now
+    if (startDiffInDays == 0) {
+      if (startDiffInMinutes == 0) {
+        startSubtitle = "Event Starts Now!";
+      } // if
+
+      else if (startDiffInMinutes < 60) {
+        startSubtitle = "Started $startDiffInMinutes minutes ago.";
+      } // if
+
+      else if (startDiffInHours <= 24) {
+        startSubtitle = "Started $startDiffInHours hours ago.";
+      } // if
+
+      else {
+        // This should never be reached
+        startSubtitle = "Start $startDiffInHours hours ago.";
+      } // else
+    } // if
+
+    // In the Future
+    else if (startDiffInDays < 0) {
+      if (startDiffInDays == -1) {
+        // Starts tomorrow at [Insert Time]
+        startSubtitle = "Starts tomorrow at";
+      } // if
+
+      else if (startDiffInDays < -7) {
+        // Starts in ${startDiffInDays} at [Time]
+        startSubtitle = "Starts in $startDiffInDays at";
+      } // if
+
+      else if (startDiffInDays == -7) {
+        // Starts next ${DayOfWeek} at [Insert Time]
+        startSubtitle = "Starts next ${this.start.weekday} at";
+      } // if
+
+      else {
+        // Starts on [Date] at [Time]
+        startSubtitle = "Starts on [Date] at [Time]";
+      } // else
+    } // else if
+
+    // In the Past
+    else {
+      if (startDiffInDays == 1) {
+        // Started yesterday since [Insert Time]
+        startSubtitle = "Started yesterday since";
+      } // if
+
+      else {
+        // Started since [Date] at [Time]
+        startSubtitle = "Started since";
+      } // if
+    } // else
+
+    // Compare start time to now
+    final int endDiffInDays = myCurrent.difference(this.end).inDays;
+    final int endDiffInHours = myCurrent.difference(this.end).inHours;
+    final int endDiffInMinutes = myCurrent.difference(this.end).inMinutes;
 
     if (this.end != null) {
-      if (myCurrent.difference(this.end).inDays == 0) {
-        formattedEndDate = 'today';
-      } else if (myCurrent.difference(this.end).inDays == 1) {
-        formattedEndDate = 'yesterday';
-      } else if (myCurrent.difference(this.end).inDays == -1) {
-        formattedEndDate = 'tomorrow';
-      } else {
-        formattedEndDate = 'on ${event.endDate}';
-      }
-    }
+      // Event ends today
+      if (endDiffInDays == 0) {
+        if (endDiffInMinutes == 0) {
+          endSubtitle = "Event is about to end!";
+        } // if
 
-    if (myCurrent.isBefore(this.start)) {
-      startSubtitle = 'Starts $formattedStartDate at ${event.startTime}';
-      if (event.endDate.trim().isNotEmpty)
-        endSubtitle = 'Ends $formattedEndDate at ${event.endTime}';
-    } else if (myCurrent.isAtSameMomentAs(start)) {
-      startSubtitle = 'Event starts now!';
-      if (event.endDate.trim().isNotEmpty)
-        endSubtitle = 'Ends at ${event.endTime} $formattedEndDate';
-    } else if (myCurrent.isAfter(this.start)) {
-      if (this.end != null) {
-        if (myCurrent.isBefore(this.end)) {
-          startSubtitle =
-              'Started since ${event.startTime} ${this.formattedStartDate}';
-          endSubtitle = 'Ends at ${event.endTime} $formattedEndDate';
-        } else {
-          startSubtitle = 'Ended since ${event.endTime} $formattedEndDate ';
-        }
-      } else {
-        startSubtitle =
-            'Started since ${event.startTime} ${this.formattedStartDate}';
-      }
-    } else {
-      startSubtitle = 'Ended since ${event.startTime} $formattedEndDate ';
-    }
+        else if (endDiffInMinutes < -60) {
+          endSubtitle = "Ends in $endDiffInMinutes minutes.";
+        } // if
+
+        else if (endDiffInHours <= -24) {
+          endSubtitle = "Ends in $startDiffInHours hours.";
+        } // if
+
+        else {
+          // This should never be reached
+          endSubtitle = "Ends [Date] at [Time]"; 
+        } // else
+      } // if
+
+      // In the Future
+      else if (endDiffInDays < 0) {
+        if (endDiffInDays == -1) {
+          // Ends tomorrow at [Insert Time]
+          endSubtitle = "Ends tomorrow at";
+        } // if
+
+        else if (endDiffInDays < -7) {
+          // Ends in ${startDiffInDays} at [Time]
+          endSubtitle = "Ends in $startDiffInDays days at";
+        } // if
+
+        else if (endDiffInDays == -7) {
+          // Ends next ${DayOfWeek} at [Insert Time]
+          endSubtitle = "Ends next ${this.start.weekday} at";
+        } // if
+
+        else {
+          // Ends on [Date] at [Time]
+          endSubtitle = "Ends [Date] at [Time]";
+        } // else
+      } // else if
+
+      // In the Past
+      else {
+        if (endDiffInDays == 1) {
+          // Started yesterday since [Insert Time]
+          endSubtitle = "Ended yesterday since";
+        } // if
+
+        else {
+          // Started since [Date] at [Time]
+          endSubtitle = "Ends [Date] at [Time]";
+        } // if
+      } // else
+    }// else 
+    
+    else {
+      endSubtitle = "";
+    } // else
   } // _formatEventDatesAndTimes
 
   @override
@@ -92,8 +174,6 @@ class FetchFullEventSuccess extends FetchFullEventState {
         start,
         endSubtitle,
         startSubtitle,
-        formattedEndDate,
-        formattedStartDate,
       ];
 } // FetchEventSuccess
 
