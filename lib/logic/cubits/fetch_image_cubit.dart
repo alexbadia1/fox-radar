4f95359cf94c916c695df4858b062d972766a526
abadia2 <abadia2@illinois.edu>
2024-03-27 05:47:13 -0500

    Enhance search

--- lib/logic/cubits/fetch_image_cubit.dart
+++ lib/logic/cubits/fetch_image_cubit.dart
@@ -1,8 +1,18 @@
 import 'dart:typed_data';
 import 'package:bloc/bloc.dart';
-import  'fetch_image_state.dart';
+import 'fetch_image_state.dart';
 import 'package:database_repository/database_repository.dart';
 
+/**
+ * 
+ * TODO: FIX
+ * 
+ * In a list where items (and their associated Cubits) could be destroyed at 
+ * any time, it's risky to have Cubits that perform asynchronous actions. When 
+ * the Cubit is destroyed, any ongoing asynchronous actions could still 
+ * complete and try to update the Cubit, leading to errors.
+ * 
+ */
 class FetchImageCubit extends Cubit<FetchImageState> {
   final DatabaseRepository db;
   FetchImageCubit({required this.db}) : super(FetchImageInitial());
@@ -17,8 +27,7 @@ class FetchImageCubit extends Cubit<FetchImageState> {
       } else {
         emit(FetchImageFailure());
       }
-    }
-    catch (error) {
+    } catch (error) {
       emit(FetchImageFailure());
     }
     return;
